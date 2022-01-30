cdff0913f353348215a8acbb1ef374f426b6fd88
amirali <amiraliyavari785@gmial.com>
2022-01-30 03:35:01 -0800

    config: install tailwindcss

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="h-[100vh] flex flex-row w-[200vw] bg-MainBackground bg-no-repeat bg-cover">
+    <MainPage1 />
+    <AboutTeamPage />
+  </div>
+</template>
+<script>
+import MainPage1 from "./MainPageSummary.vue";
+import AboutTeamPage from "./AboutTeamPage.vue";
+export default {
+  components: {
+    MainPage1,
+    AboutTeamPage,
+  },
+};
+</script>
