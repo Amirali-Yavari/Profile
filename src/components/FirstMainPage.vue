17ebbc666f56e0edfe114578cf3e5c46758d9df7
amirali <amiraliyavari785@gmail.com>
2022-01-31 12:17:14 -0800

    add read more button and make only template of scroll right

--- src/components/FirstMainPage.vue
+++ src/components/FirstMainPage.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="h-[98vh] w-1/2 relative">
-    <div class="absolute mb-1 bottom-16 right-10 cursor-pointer text-orange-500 font-bold">scroll right</div>
+    <div
+      class="absolute mb-1 bottom-16 right-10 cursor-pointer text-orange-500 font-bold"
+    >
+      Scroll Right
+    </div>
     <div class="p-10 flex flex-col gap-4">
       <MyLogo />
       <MyTitle />
       <MySummary />
+      <ReadMore />
     </div>
 
     <NavbarBottom />
@@ -13,12 +18,14 @@
 <script>
 import MyTitle from "./MainPage/MyTitle.vue";
 import MySummary from "./MainPage/MySummary.vue";
+import ReadMore from "./MainPage/ReadMore.vue";
 import NavbarBottom from "./NavbarBottom.vue";
 import MyLogo from "./MainPage/MyLogo.vue";
 export default {
   components: {
     MyTitle,
     MySummary,
+    ReadMore,
     MyLogo,
     NavbarBottom,
   },
---
+++ src/components/MainPage/ReadMore.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="w-[45%] h-12 bg-black text-white flex items-center justify-center text-xl cursor-pointer uppercase hover:bg-orange-500 transition-all">Read More</div>
+</template>
