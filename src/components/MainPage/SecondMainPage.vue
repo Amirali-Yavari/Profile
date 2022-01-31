10f5a132ad62f7b2b59e8ef70649cf5e3dd5499e
amirali <amiraliyavari785@gmail.com>
2022-01-31 13:21:23 -0800

    make navbar template again remake the picture background

--- src/components/MainPage/SecondMainPage.vue
+++ src/components/MainPage/SecondMainPage.vue
@@ -2,23 +2,27 @@
   <div class="h-full w-1/2 relative">
     <div
       @click="ScrollLeft"
-      class="absolute mb-1 bottom-3 left-9 cursor-pointer text-pink-500 font-bold select-none"
+      class="absolute mb-1 bottom-4 left-12 cursor-pointer text-pink-500 font-bold select-none"
     >
       Scroll left
     </div>
-    <img src="./../../../public/MyImage/vv-min.jpg" class="object-cover h-[97%]" alt="">
+    <img
+      src="./../../../public/MyImage/pic1.png"
+      class="object-cover h-[97%] ml-10"
+      alt=""
+    />
   </div>
 </template>
 <script>
 export default {
-  methods:{
-    ScrollLeft(){
+  methods: {
+    ScrollLeft() {
       window.scrollTo({
         top: 0,
         left: -100000,
         behavior: "smooth",
       });
-    }
+    },
   },
   mounted() {
     setTimeout(() => {
--- src/components/NavbarBottom.vue
+++ src/components/NavbarBottom.vue
@@ -1,8 +1,13 @@
 <template>
   <div
-    class="w-full h-16 flex flex-row justify-center gap-8 fixed bottom-0 bg-white"
+    class="w-full h-16 flex flex-row-reverse justify-between items-center gap-8 fixed bottom-0 bg-white"
   >
-    NavbarBottom
+    <div class="w-10 h-10 ring-1 ring-gray-300 rounded-md flex items-center justify-center mr-2">
+      X
+    </div>
+    <div class="text-pink-500 text-3xl ml-2">
+      Yavari
+    </div>
   </div>
 </template>
 <script></script>
