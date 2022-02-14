a4242db2b587d617514417fe279e373f68f3c086
amirali <amiraliyavari785@gmail.com>
2022-02-14 11:43:12 -0800

    add tips for know the users that can swipe

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
+  <transition name="tip">
+    <TipStarter v-if="showTip" />
+  </transition>
+
   <router-view v-slot="{ Component }">
     <transition name="Slide" mode="out-in">
       <component :is="Component" :key="$route.path"></component>
     </transition>
   </router-view>
 </template>
-
+<script>
+import TipStarter from "./components/TipStarter.vue";
+import { ref } from "vue";
+export default {
+  setup() {
+    const showTip = ref(true);
+    setTimeout(() => {
+      showTip.value = false;
+    }, 7000);
+    return { showTip };
+  },
+  components: {
+    TipStarter,
+  },
+};
+</script>
 <style>
+.tip-leave-from {
+  opacity: 1;
+  transition: all 0.5s ease;
+  transform: translateY(0px);
+}
+.tip-leave-to {
+  opacity: 0;
+  transform: translateY(-100px);
+  transition: all 0.5s ease;
+}
 .Slide-enter-from {
   opacity: 0;
   transition: all 0.5s ease;
---
+++ src/components/TipStarter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="h-[15vh] sm:w-1/2 w-[90%] bg-amber-400/90 rounded-md text-lg sm:text-xl flex flex-col gap-5 items-center justify-center z-40 fixed top-0 right-[5%] sm:right-[25%] p-1"
+  >
+    <div class="hidden sm:block text-white">
+      You can change product by swipe to right or left
+    </div>
+    <div class="text-white sm:hidden">
+      you can use swipe to right or left in product and main page and contactUs
+      for change page or product
+    </div>
+  </div>
+</template>
